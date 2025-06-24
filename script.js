f073bf2d796dd0cd83b980c728b297e342fee458
AKArnaiz <82615204+AKArnaiz@users.noreply.github.com>
2025-06-24 21:00:36 +1200

    Update script.js

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 //typed js
 
 const typed = new Typed('.multiple-text', {
-    strings: ['IT professional', 'Support Analyst', 'Consultant', 'Web Developer', 'Man united fan', 'Community volunteer'],
+    strings: ['IT professional', 'Support Analyst', 'Consultant', 'Web Developer', 'Man united fan', 'Community volunteer', 'change analyst'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
